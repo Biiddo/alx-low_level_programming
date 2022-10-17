7a70c44488bc4349b5c9aa8e813d95daf5472f65
Biido <abiodunadejumo77@gmail.com>
2022-10-17 16:13:29 -0700

    3 alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers gors there */
+/* more headers goes there */
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -20,9 +20,9 @@ int main(void)
 	/* your code goes there */
 	m = n % 10;
 	if (m > 5)
-		printf("Last digit of %d is %d  and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	if (m == 0)
-		printf("Last digit of %d is %d and is zero\n", n, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
