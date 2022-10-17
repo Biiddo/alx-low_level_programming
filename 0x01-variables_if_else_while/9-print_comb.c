952fa8671750179679a2032141d2035bd4f4aef2
Biido <abiodunadejumo77@gmail.com>
2022-10-17 15:44:50 -0700

    9comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,49 +1,24 @@
-nclude <stdio.h>
-
-
-
+#include <stdio.h>
 /**
+ * main - Entry Point
  *
- * * main - print 0-9 separated with commas, uding putchar
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
+ * Return: Alwats 0 (success)
+ */
 
 int main(void)
-
 {
+	int a;
 
-		int n = '0';
-
-
-
-			while (n <= '9')
-
-					{
-
-								putchar(n);
-
-										if (n != '9')
-
-													{
-
-																	putchar(',');
-
-																				putchar(' ');
-
-																						}
-
-												n++;
-
-													}
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	for (a = 0; a < 100; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
